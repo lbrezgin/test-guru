298e7770b87e2d649df90909bc7248085b145a00
lbrezgin <lewickbrez@gmail.com>
2023-09-13 11:41:04 +0300

    refactor: change seed file and user models method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests
 
-  def tests_by_difficulty(difficulty)
-    tests.where(level: difficulty)
+  def tests_by_level(level)
+    tests.where(level: level)
   end
 end
