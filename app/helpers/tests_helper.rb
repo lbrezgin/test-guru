914b5762d961937bdc7eed9dc78340c60e73e4db
Lev Brezgin <lbrezgin@Levs-MacBook-Pro.local>
2023-11-21 22:36:27 +0200

    feat: add all from video up 9part 4 theme

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,16 @@
 module TestsHelper
+  def test_level(test)
+    case test.level
+    when 0
+      'easy'
+    when 1
+      'elementary'
+    when 2
+      'advanced'
+    when 3
+      'hard'
+    else
+      'hero'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
-  has_many :tests_users
-  has_many :tests, through: :tests_users
+  has_many :test_passages
+  has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test', foreign_key: 'user_id'
 
   validates :email, presence: true
   def tests_by_level(level)
     tests.by_level(level)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
