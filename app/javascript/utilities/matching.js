2e74cfef3f4b163f4f6b3d098fc04a088f2254bf
lewick <lewickbrez@gmail.com>
2024-01-24 09:59:10 +0200

    refactor: if refactor

--- app/javascript/utilities/matching.js
+++ app/javascript/utilities/matching.js
@@ -9,15 +9,12 @@ function checkPasswordMatch() {
     var password_match_message = document.getElementById("password-match-message");
 
 
-    if (password_confirmation.value !== "") {
-        if (password.value === password_confirmation.value) {
-            password_match_message.innerHTML = '<p style="color: green;">Passwords match</p>'
-
-        } else {
-            password_match_message.innerHTML = '<p style="color: red;">Passwords not match</h1>'
-        }
-    } else {
+    if (password_confirmation.value === "" || password.value === "") {
         password_match_message.innerHTML = ""
+    } else if (password.value === password_confirmation.value) {
+        password_match_message.innerHTML = '<p style="color: green;">Passwords match</p>'
+    } else {
+        password_match_message.innerHTML = '<p style="color: red;">Passwords not match</h1>'
     }
 }
 
