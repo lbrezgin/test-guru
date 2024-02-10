a560ae7f557d496b39047830de417b19e9dee9f5
lewick <lewickbrez@gmail.com>
2024-02-10 19:50:59 +0200

    refactor: new edits

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,9 +1,2 @@
 module TestPassagesHelper
-  def actual_question(test_passage)
-    for i in 0..test_passage.test.questions.count
-      if (test_passage.test.questions[i] == test_passage.current_question)
-        return i+1
-      end
-    end
-  end
 end
