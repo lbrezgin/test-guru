4491eb608cb8fcdca2251adad6d1c6d821934c8c
lewick <lewickbrez@gmail.com>
2024-02-21 21:58:59 +0200

    commented Dotenv::Railtie.load line

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,6 @@ module TestGuru
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    Dotenv::Railtie.load
+    #Dotenv::Railtie.load
   end
 end
