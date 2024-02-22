4118ca9ee5fa71a95aeb76c08c6f51bbb85a9dcc
lewick <lewickbrez@gmail.com>
2024-02-22 15:16:08 +0200

    db seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ Category.create(title: 'Ruby on Rails')
 Category.create(title: 'Python')
 Category.create(title: 'Java')
 
-Admin.create(first_name: "Admin", last_name: "Admin", email: "admin@gmail.com", password: "123456")
+Admin.create(first_name: "Admin", last_name: "Admin", email: "lewickbrez@gmail.com", password: "654321")
