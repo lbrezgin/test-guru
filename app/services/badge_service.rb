f28c136602b8df7c180c57ac9e913fe83783a151
lewick <lewickbrez@gmail.com>
2024-03-17 00:20:19 +0200

    refactor: edits 5

--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -25,19 +25,18 @@ class BadgeService
   end
 
   def all_test_category(category_title)
-    category_tests = Test.test_names_by_category(category_title)
-    user_tests_by_category = @user.test_passages.where(successful: true).map do |pass|
-      pass.test if category_tests.include?(pass.test)
-    end
-    category_tests.count == user_tests_by_category.uniq.count
+    category_tests_ids = Test.test_names_by_category(category_title).ids
+    check(category_tests_ids)
   end
 
   def all_levels(level)
-    level_tests = Test.all.where(level: level.to_i)
-    user_tests_by_level = @user.test_passages.where(successful: true).map do |pass|
-      pass.test if level_tests.include?(pass.test)
-    end
-    level_tests == user_tests_by_level.uniq.count
+    level_tests_ids = Test.by_level(level.to_i).ids
+    check(level_tests_ids)
+  end
+
+  def check(all_tests_by_rule)
+    user_tests_ids = @user.test_passages.where(successful: true, test_id: all_tests_by_rule).pluck(:test_id).uniq
+    all_tests_by_rule.count == user_tests_ids.count
   end
 end
 
